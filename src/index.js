2ed54e84778baab7a501a16e6df0a51d2f8161ab
saurabhrkt <chaurasiasaurabh299@gmail.com>
2025-02-17 22:44:50 +0530

    utills code

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./env"
 });  
-connectToMongoDB();
+connectToMongoDB()
+.then(()=>{
+
+  app.on("error", (err)=>{
+    console.error("Express error: ", err);
+  })
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server is running on port: ${process.env.PORT||8000}`);
+  })
+})
+.catch((err)=>{
+  console.error("Error connecting to MongoDB: ", err);
+  process.exit(1);
+})
